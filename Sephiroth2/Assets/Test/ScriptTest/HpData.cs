ae6c7576298fb15d30faca79ba0cf7fbd26b0162
alals93vu6 <a0987915131@gmail.com>
2023-01-17 18:48:17 +0800

    HP UI Test

---
+++ Sephiroth2/Assets/Test/ScriptTest/HpData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace Project.PlayerHpData
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "Playerdata_HP", order = 0)]
+    
+    [System.Serializable]
+    public class HpData : ScriptableObject
+    {
+        [SerializeField] public Image ShowPlayerHP;
+        [SerializeField] public float ShowHPFloat;
+        [SerializeField] public float MaxHP;
+        [SerializeField] public float NowHP;
+    }
+}
+
---
+++ Sephiroth2/Assets/Test/ScriptTest/HpTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project.PlayerHpData;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpTest : MonoBehaviour
+{
+    [SerializeField] public HpData _hpData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hpData.ShowPlayerHP = GameObject.Find("HpShow").GetComponent<Image>();
+        _hpData.NowHP =  _hpData.MaxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _hpData.NowHP -= 10f;
+        }
+
+        if ( _hpData.NowHP <= 0)
+        {
+            _hpData.NowHP = 0f;
+        }
+
+        _hpData.ShowHPFloat =  _hpData.NowHP /  _hpData.MaxHP;
+
+        _hpData.ShowPlayerHP.fillAmount = Mathf.Lerp( _hpData.ShowPlayerHP.fillAmount,  _hpData.ShowHPFloat, 0.02f);
+    }
+
+  
+}
