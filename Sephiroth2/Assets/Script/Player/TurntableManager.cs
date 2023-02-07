3d7d6b5e4ade9896da78c30cea9baecc628f2a6c
alals93vu6 <a0987915131@gmail.com>
2023-02-08 06:55:00 +0800

    Summon Draft Part.2

--- Sephiroth2/Assets/Script/Player/TurntableManager.cs
+++ Sephiroth2/Assets/Script/Player/TurntableManager.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class TurntableManager : MonoBehaviour
 {
+    [Header("物件")]
     [SerializeField] public int SummonState;
+    [SerializeField] public GameObject[] Summoner;
+    //[SerializeField] public GameObject[] SummonerTurntable;
+    [Header("Offset")]
+    [SerializeField] private Vector3 SummonerOffset;
+    [SerializeField] private Vector3 TurntableOffset;
     // Start is called before the first frame update
     void Start()
     {
         SummonState = 0;
+        
+        PlayerPrefs.SetInt("NowSummonA",1);
+        PlayerPrefs.SetInt("NowSummonB",2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnPlayerSummon();
+        }
     }
 
     public void SimonCheck()
@@ -25,20 +37,13 @@ public class TurntableManager : MonoBehaviour
     public void OnPlayerSummon()
     {
         int NowSummon = (SummonState == 1) ? PlayerPrefs.GetInt("NowSummonA") : (SummonState == 2) ? PlayerPrefs.GetInt("NowSummonB") : 0;
-
-        switch (NowSummon)
-        {
-            case 0:
-                GameObject.FindWithTag("Summoner");
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-        }
+        Destroy(GameObject.FindWithTag("Summoner"));
+        //Destroy(GameObject.FindWithTag("SummonerTurntable"));
+        Instantiate(Summoner[NowSummon], this.transform.position, this.transform.rotation);
+        //Instantiate(SummonerTurntable[NowSummon], this.transform.position, this.transform.rotation);
     }
-    
-    
+    /*
+    var NowSummoner = GameObject.FindWithTag("Summoner");
+    var NowSummonerSkill = GameObject.FindWithTag("SummonerTurntable");
+    */
 }
