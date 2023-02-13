2ec9d03f1b03e9a1f096ed48e4d83567111a787e
YouQi <alexjohn2109a@gmail.com>
2023-02-14 02:20:16 +0800

    Creat_Effect

---
+++ Sephiroth2/Assets/Effect/Buff/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    [Header("刪除時間")]
+    [SerializeField] public float Destroy_Time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, Destroy_Time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Sephiroth2/Assets/Effect/Creat_Effect_Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creat_Effect_Player : MonoBehaviour
+{
+    public GameObject Buff_pos;
+
+    public GameObject Buff_Recover;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Z");
+            Creat_Effect(Buff_Recover);
+        }
+    }
+    public void Creat_Effect(GameObject Effect)
+    {
+        Instantiate(Effect, Buff_pos.transform.position, new Quaternion(0, 0, 0, 0));
+    }
+}
