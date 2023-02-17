18232b4b2fcec76ba056b8ed3265a152369bec03
YouQi <alexjohn2109a@gmail.com>
2023-02-17 14:15:10 +0800

    Add Shake_camera_attack

---
+++ Sephiroth2/Assets/Effect/Shake_Camera/Shake_Camera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake_Camera : MonoBehaviour
+{
+
+    /*#region Instance
+    static public Shake instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion */
+
+    [SerializeField] public bool start_Shake = false;
+    [Header("晃動幅度")]
+    [SerializeField] public AnimationCurve curve;
+    [Header("晃動時長")]
+    [SerializeField] public float duration;
+
+
+    void Update()
+    {
+        if (start_Shake)
+        {
+            start_Shake = false;
+            StartCoroutine(Shaking());
+        }
+    }
+
+    IEnumerator Shaking()
+    {
+        Vector3 startPosition = Camera.main.transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            Camera.main.transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        Camera.main.transform.position = startPosition;
+    }
+}
+
