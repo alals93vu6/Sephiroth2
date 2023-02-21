a6e7c26cf17268007e8be9a1e265a58a6bd5daf4
YouQi <alexjohn2109a@gmail.com>
2023-02-21 22:08:54 +0800

    Add System_Location

--- Sephiroth2/Assets/Scirpt/System_Location/System_Location.cs
+++ Sephiroth2/Assets/Scirpt/System_Location/System_Location.cs
@@ -5,14 +5,46 @@ using UnityEngine;
 public class System_Location : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] protected GameObject[] Player_Location = new GameObject[3];
+    [SerializeField] protected GameObject[] Enemy_Location = new GameObject[3];
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Location_Update();
+    }
+    void Location_set(GameObject[] gameObjects)
+    {
+        if (gameObjects[0] == null)
+        {
+            if (gameObjects[1] != null)
+            {
+                gameObjects[0] = gameObjects[1];
+                gameObjects[1] = null;
+                if (gameObjects[2] != null)
+                {
+                    gameObjects[1] = gameObjects[2];
+                    gameObjects[2] = null;
+                }
+            }
+        }
+
+        if (gameObjects[1] == null)
+        {
+            if (gameObjects[2] != null)
+            {
+                gameObjects[1] = gameObjects[2];
+                gameObjects[2] = null;
+            }
+        }
+    }
+    void Location_Update()
+    {
+        Location_set(Player_Location);
+        Location_set(Enemy_Location);
     }
 }
