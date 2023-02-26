9568b49bc6790e24a2d828cd20e2ca8b6b07b551
YouQi <alexjohn2109a@gmail.com>
2023-02-26 14:40:35 +0800

    add SetActive_Button

---
+++ Sephiroth2/Assets/Map/Map_System/SetActiveButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveButton : MonoBehaviour
+{
+    public static bool is_end = false;
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        is_end = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(is_end);
+        SHOWHIDE();
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            is_end = true;
+        }
+    }
+    void SHOWHIDE()
+    {
+        if (is_end == false)
+        {
+            button.SetActive(false);
+        }
+        else
+        {
+            button.SetActive(true);
+        }
+    }
+}
+
+
