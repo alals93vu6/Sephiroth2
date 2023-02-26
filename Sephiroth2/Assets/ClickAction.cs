bfb7c471304ce454b4ea2cc528b0def8489fef85
alals93vu6 <a0987915131@gmail.com>
2023-02-27 02:25:28 +0800

    Change Map Click

---
+++ Sephiroth2/Assets/ClickAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider))]
+public class ClickAction : MonoBehaviour
+{
+    public UnityEvent Onclick;
+
+    private void OnMouseDown()
+    {
+        Onclick.Invoke();
+    }
+}
