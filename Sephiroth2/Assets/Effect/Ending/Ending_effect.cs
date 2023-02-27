aab9a065a9679bed98e180e54f36eab1b7682974
YouQi <alexjohn2109a@gmail.com>
2023-02-27 16:03:08 +0800

    add creat_ending

---
+++ Sephiroth2/Assets/Effect/Ending/Ending_effect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending_effect : MonoBehaviour
+{
+    public GameObject win;
+    public GameObject dead;
+    public bool is_win = false, is_dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        end();
+    }
+
+    void end()
+    {
+        if (is_win)
+        {
+            Instantiate(win);
+            is_win = false;
+        }
+        if (is_dead)
+        {
+            Instantiate(dead);
+            is_dead = false;
+        }
+    }
+}
