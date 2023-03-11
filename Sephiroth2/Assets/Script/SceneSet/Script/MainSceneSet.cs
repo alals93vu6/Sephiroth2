692da9688e9cf09c6870a7565c8ca620eac5740b
YouQi <alexjohn2109a@gmail.com>
2023-03-11 22:29:49 +0800

    Update Gameing_setting

--- Sephiroth2/Assets/Script/SceneSet/Script/MainSceneSet.cs
+++ Sephiroth2/Assets/Script/SceneSet/Script/MainSceneSet.cs
@@ -7,24 +7,12 @@ using UnityEngine.EventSystems;
 public class MainSceneSet : MonoBehaviour
 {
     public int GamePlay_scenes_ID;
-    /* [Tooltip("載入畫面編號")]
-     [Header("載入畫面編號")]
-     [SerializeField] private List<int> SceneIdex = new List<int>();*/
-
-    /* [Header("按鈕當前位置")]
-     [Range(0, 2)][SerializeField] public int MainSceneNum;
-     [Header("提示提示位置")]
-     [SerializeField] public List<Vector3> TipRectTransform = new List<Vector3>();
-     [Header("提示物件")]
-     [SerializeField] public RectTransform TipGameObject;*/
     private void Start()
     {
 
     }
     private void Update()
     {
-        // Chang_TipTransform(TipGameObject.localPosition, 5);
-        //keyset();
     }
     public async void LoadScene_Scene(int SceneID)
     {
@@ -32,8 +20,7 @@ public class MainSceneSet : MonoBehaviour
         SceneManager.LoadScene(SceneID);
         if (SceneID == GamePlay_scenes_ID)
         {
-            //Cursor.visible = false;
-            SetAudio.audioNum = 1;
+            SetAudio.audioNum = 0;
         }
 
     }
@@ -41,52 +28,4 @@ public class MainSceneSet : MonoBehaviour
     {
         Application.Quit();
     }
-
-    /*  void Chang_TipTransform(Vector3 now_vect3, float speed)
-      {
-          now_vect3 = TipGameObject.localPosition;
-
-          TipGameObject.localPosition = Vector3.Lerp(now_vect3, TipRectTransform[MainSceneNum], speed * Time.deltaTime);
-
-
-          if (MainSceneNum > TipRectTransform.Count)
-          {
-              MainSceneNum = 0;
-          }
-          if (MainSceneNum < 0)
-          {
-              MainSceneNum = 0;
-          }
-      }
-      /* void keyset()
-       {
-
-           if (Input.GetKeyDown(KeyCode.DownArrow))
-           {
-               if (MainSceneNum == TipRectTransform.Count - 1)
-               {
-                   MainSceneNum = 0;
-               }
-               else
-               {
-                   MainSceneNum = MainSceneNum + 1;
-               }
-
-           }
-           if (Input.GetKeyDown(KeyCode.UpArrow))
-           {
-               if (MainSceneNum == 0)
-               {
-                   MainSceneNum = TipRectTransform.Count - 1;
-               }
-               else
-               {
-                   MainSceneNum = MainSceneNum - 1;
-               }
-           }
-           if (Input.GetKeyDown(KeyCode.KeypadEnter))
-           {
-               SceneManager.LoadScene(SceneIdex[SceneIdex[MainSceneNum]]);
-           }
-       }*/
 }
---
+++ Sephiroth2/Assets/Script/SceneSet/Script/Setting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Setting : MonoBehaviour
+{
+    public static bool is_show_setting = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(is_show_setting);
+        esc();
+    }
+
+    void set_bool()
+    {
+        while (is_show_setting)
+        {
+            SceneManager.LoadScene(5);
+            break;
+        }
+        while (!is_show_setting)
+        {
+            SceneManager.LoadScene(4);
+            break;
+        }
+    }
+
+    void esc()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            is_show_setting = !is_show_setting;
+            set_bool();
+        }
+    }
+   public  void LoadScene(int SceneID)
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+}
