a9e38618c5a37c90a658aa8ae2dda372d1253654
alals93vu6 <a0987915131@gmail.com>
2023-03-13 16:13:17 +0800

    On Assigned System Part.1

---
+++ Sephiroth2/Assets/Script/GamePlaying/UI/AssignedDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignedDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject disPlayIcon;
+
+    public void OnAssigned()
+    {
+        disPlayIcon.SetActive(false);
+    }
+
+    public void ExitAssigned()
+    {
+        disPlayIcon.SetActive(true);
+    }
+}
---
+++ Sephiroth2/Assets/Script/GamePlaying/UI/SummonAssigendDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonAssigendDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject[] disPlayIcon;
+
+    public void OnAssigned()
+    {
+        disPlayIcon[0].SetActive(false);
+    }
+
+    public void ExitAssigned()
+    {
+        disPlayIcon[0].SetActive(true);
+    }
+
+    public void OnChangeIcon()
+    {
+        disPlayIcon[0] = disPlayIcon[2];
+        disPlayIcon[1] = disPlayIcon[3];
+    }
+}
